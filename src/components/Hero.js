d57159cdade8bf932b50047b651c8e3bc8b598f8
Sivapriya2 <113622027+Sivapriya2@users.noreply.github.com>
2023-10-07 21:20:18 +0530

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,7 +3,7 @@ import './Hero.css'
 function Hero(props) {
   return (
     <div className={props.cName}>
-      <img src={props.img} alt='image'></img>
+      <img src={props.img} alt='nature'></img>
       <div className='hero-text'>
       <h1>{props.title}</h1>
       <p>{props.text}</p>

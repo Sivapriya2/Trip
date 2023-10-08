2ecba92e3e7bdb4ab2073bc9c803986f0f6798f8
Sivapriya2 <113622027+Sivapriya2@users.noreply.github.com>
2023-10-08 14:33:12 +0530

    Update script.js

--- src/components/script.js
+++ src/components/script.js
@@ -1,15 +1,15 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycby2Q22Rddmthrqlpnd8EjKryY_MYYxI8MTvL6HXPc_TohS5zi5umkxHviwAz857XdnB/exec'
-        const form = document.forms['submit-to-google-sheet']
-        const msg = document.getElementById("msg");
-        form.addEventListener('submit', e => {
-          e.preventDefault()
-          fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-            .then(response => {
-                msg.innerHTML = "Message sent successfully"
-                setTimeout(function(){
-                    msg.innerHTML=""
-                },5000)
-                form.reset()
-            })
-            .catch(error => console.error('Error!', error.message))
-        })
+        // const form = document.forms['submit-to-google-sheet']
+        // const msg = document.getElementById("msg");
+        // form.addEventListener('submit', e => {
+        //   e.preventDefault()
+        //   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        //     .then(response => {
+        //         msg.innerHTML = "Message sent successfully"
+        //         setTimeout(function(){
+        //             msg.innerHTML=""
+        //         },5000)
+        //         form.reset()
+        //     })
+        //     .catch(error => console.error('Error!', error.message))
+        // })
